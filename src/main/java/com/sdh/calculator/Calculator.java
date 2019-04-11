059e7973bbf14776bff3501cdad73442d3be6193
Donghyun <tbc01204@gmail.com>
2019-04-11 14:49:03 +0900

    Add sub, mul, div fucntion

--- src/main/java/com/sdh/calculator/Calculator.java
+++ src/main/java/com/sdh/calculator/Calculator.java
@@ -7,4 +7,16 @@ public class Calculator {
 	int sum(int a, int b) {
 		return a + b;
 	}
+
+	int sub(int a, int b) {
+		return a - b;
+	}
+
+	int mul(int a, int b) {
+		return a * b;
+	}
+
+	int div(int a, int b) {
+		return a / b;
+	}
 }
--- src/test/java/com/sdh/calculator/CalculatorTest.java
+++ src/test/java/com/sdh/calculator/CalculatorTest.java
@@ -10,4 +10,19 @@ public class CalculatorTest {
 	public void testSum() {
 		assertEquals(5, calculator.sum(2,3));
 	}
+
+	@Test
+	public void testSub() {
+		assertEquals(-1, calculator.sub(2,3));
+	}
+
+	@Test
+	public void testMul() {
+		assertEquals(6, calculator.mul(2,3));
+	}
+
+	@Test
+	public void testDiv() {
+		assertEquals(3, calculator.div(6,2));
+	}
 }
