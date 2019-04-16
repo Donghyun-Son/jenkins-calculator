aa0bb7c834e0fe275c273084bf596f07ca6e1d46
Donghyun <tbc01204@gmail.com>
2019-04-16 15:27:31 +0900

    Add checkstyle

--- src/main/java/com/sdh/calculator/Calculator.java
+++ src/main/java/com/sdh/calculator/Calculator.java
@@ -2,20 +2,41 @@ package com.sdh.calculator;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * This is Javadoc sample
+ * @author SDH
+ */
 @Service
 public class Calculator {
+	/** calculate sum
+	 * @param a
+	 * @param b
+	 * @return int
+	 */
 	int sum(int a, int b) {
 		return a + b;
 	}
-
+	/** calculate sub
+	 * @param a
+	 * @param b
+	 * @return int
+	 */
 	int sub(int a, int b) {
 		return a - b;
 	}
-
+	/** calculate mul
+	 * @param a
+	 * @param b
+	 * @return int
+	 */
 	int mul(int a, int b) {
 		return a * b;
 	}
-
+	/** calculate div
+	 * @param a
+	 * @param b
+	 * @return int
+	 */
 	int div(int a, int b) {
 		return a / b;
 	}
--- src/main/java/com/sdh/calculator/CalculatorApplication.java
+++ src/main/java/com/sdh/calculator/CalculatorApplication.java
@@ -3,9 +3,15 @@ package com.sdh.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is Javadoc sample
+ * @author SDH
+ */
 @SpringBootApplication
 public class CalculatorApplication {
-
+	/** run Application
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
